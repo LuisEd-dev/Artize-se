f389c1b723b268ae3e6363dc748fe47f7043a8ac
LuisEd-dev <luiseduardo123321@gmail.com>
2020-09-10 13:59:00 -0300

    Manter Logado (Cookie)

--- login.php
+++ login.php
@@ -1,7 +1,7 @@
 <?php
 
 include("db/db.php");
-
+echo $_COOKIE['ManterSessao'];
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
 
     $myusername = mysqli_real_escape_string($db,$_POST['login']);
@@ -17,9 +17,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
 		
     if($count == 1) {
         echo "logado";
+
+        session_start();
         $_SESSION['login_user'] = $myusername;
          
-        header("location: index.html");
+        //header("location: index.html");
+        if(isset($_POST['check']) && $_POST['check']  == "on"){
+            setcookie("ManterSessao", $myusername, time() + 60*60*24);
+        }
     }else {
         echo  "Your Login Name or Password is invalid";
     }
