42b27c413b6dde1241bd90e3a04f9ca3addb8c20
Luís Eduardo <59936729+LuisEd-dev@users.noreply.github.com>
2020-10-02 19:22:13 -0300

    Atualização DB

--- db/artizese.sql
+++ db/artizese.sql
@@ -1,68 +1,48 @@
--- phpMyAdmin SQL Dump
--- version 4.8.5
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: 09-Set-2020 às 22:11
--- Versão do servidor: 10.1.38-MariaDB
--- versão do PHP: 7.3.2
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+CREATE TABLE `tb_confirmar` (
+  `id` int(11) NOT NULL,
+  `sessao` varchar(255) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `login` varchar(255) NOT NULL,
+  `senha` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `categoria` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `artizese`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `tb_usuarios`
---
+CREATE TABLE `tb_contato` (
+  `id` int(11) NOT NULL,
+  `telefone` varchar(30) DEFAULT NULL,
+  `celular` varchar(30) DEFAULT NULL,
+  `cidade` varchar(80) DEFAULT NULL,
+  `UF` char(2) DEFAULT NULL,
+  `mensagem` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE TABLE `tb_usuarios` (
   `id` int(11) NOT NULL,
   `login` varchar(255) NOT NULL,
   `senha` varchar(255) NOT NULL,
-  `tipo` int(11) NOT NULL
+  `nome` varchar(255) NOT NULL,
+  `categoria` varchar(30) NOT NULL,
+  `email` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Extraindo dados da tabela `tb_usuarios`
---
+ALTER TABLE `tb_confirmar`
+  ADD PRIMARY KEY (`id`);
 
-INSERT INTO `tb_usuarios` (`id`, `login`, `senha`, `tipo`) VALUES
-(1, 'admin', 'admin', 12345);
+ALTER TABLE `tb_contato`
+  ADD PRIMARY KEY (`id`);
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `tb_usuarios`
---
 ALTER TABLE `tb_usuarios`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
+ALTER TABLE `tb_confirmar`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
 
---
--- AUTO_INCREMENT for table `tb_usuarios`
---
 ALTER TABLE `tb_usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
