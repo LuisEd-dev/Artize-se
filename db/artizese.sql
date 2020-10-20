9dd7bc0d33774df5d73948adb5e91dcb80ee59b7
LuisEd-dev <luiseduardo123321@gmail.com>
2020-10-20 19:06:32 -0300

    Exportando db com sql manager

--- db/artizese.sql
+++ db/artizese.sql
@@ -1,17 +1,41 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+# SQL Manager 2007 Lite for MySQL 4.4.2.1
+# ---------------------------------------
+# Host     : localhost
+# Port     : 3306
+# Database : artizese
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES latin1 */;
+
+SET FOREIGN_KEY_CHECKS=0;
+
+CREATE DATABASE `artizese`
+    CHARACTER SET 'latin1'
+    COLLATE 'latin1_swedish_ci';
+
+USE `artizese`;
+
+#
+# Structure for the `tb_confirmar` table : 
+#
 
 CREATE TABLE `tb_confirmar` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `sessao` varchar(255) NOT NULL,
   `nome` varchar(255) NOT NULL,
   `login` varchar(255) NOT NULL,
   `senha` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `categoria` int(11) NOT NULL
+  `categoria` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+#
+# Structure for the `tb_contato` table : 
+#
 
 CREATE TABLE `tb_contato` (
   `id` int(11) NOT NULL,
@@ -19,30 +43,37 @@ CREATE TABLE `tb_contato` (
   `celular` varchar(30) DEFAULT NULL,
   `cidade` varchar(80) DEFAULT NULL,
   `UF` char(2) DEFAULT NULL,
-  `mensagem` varchar(255) DEFAULT NULL
+  `mensagem` varchar(255) DEFAULT NULL,
+  `biografia` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `tb_usuarios` (
+#
+# Structure for the `tb_img_usuario` table : 
+#
+
+CREATE TABLE `tb_img_usuario` (
   `id` int(11) NOT NULL,
+  `img` longblob,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+#
+# Structure for the `tb_usuarios` table : 
+#
+
+CREATE TABLE `tb_usuarios` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `login` varchar(255) NOT NULL,
   `senha` varchar(255) NOT NULL,
   `nome` varchar(255) NOT NULL,
   `categoria` varchar(30) NOT NULL,
-  `email` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `email` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
-ALTER TABLE `tb_confirmar`
-  ADD PRIMARY KEY (`id`);
 
-ALTER TABLE `tb_contato`
-  ADD PRIMARY KEY (`id`);
 
-ALTER TABLE `tb_usuarios`
-  ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `tb_confirmar`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
-
-ALTER TABLE `tb_usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=0;
-COMMIT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
